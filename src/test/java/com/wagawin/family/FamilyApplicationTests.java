ebdde2351a06a7ba5622b39d47dc4ac33918ab45
haytham <haytham@haytham-pc.zyxel.box>
2021-11-09 10:38:41 +0100

    basic strucutre

---
+++ src/test/java/com/wagawin/family/FamilyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wagawin.family;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
